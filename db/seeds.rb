aa42b4de74bd224600ed0579c681c0c1fe7744a1
brockcp <brockpatterson@gmail.com>
2017-10-30 09:45:23 -0700

    29_rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,12 +22,16 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create!(
+  post = Post.create!(
     user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
+
 end
 posts = Post.all
 
@@ -65,7 +69,10 @@ puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"# This file should contain all the record creation needed to seed the database with its default values.
+puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
+
+# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
